a1b45bc2af4beba827423ab7aa8ad0e30d184d2b
amir0707k <34520003+amir0707k@users.noreply.github.com>
2023-08-20 15:24:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function decimalToBinary(num) {
   //Write you code here
+	while (num>0) {
+                remainder = `${num%2}` + remainder;
+                num = Math.floor(num/2);
+            }
+            return remainder;
+  /*
+  7/2 = 1
+  3/2 = 1
   
+  
+  */
 }
 
 window.decimalToBinary = decimalToBinary;
